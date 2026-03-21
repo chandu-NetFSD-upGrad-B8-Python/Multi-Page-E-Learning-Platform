d5024ec331486086f5d580ac0afdd40643b4802d
chandu-NetFSD-upGrad-B8-Python <chanduinternship@gmail.com>
2026-03-21 15:10:01 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,73 @@
+const courses = [
+
+{ id:1, title:"HTML5",
+img:"https://media.licdn.com/dms/image/v2/D5612AQG4hJaYwE8_iQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1712977873960?e=2147483647&v=beta&t=lJWm9k-ZxlrGa_JrNQ_28HRmc8w-eajazsKuzHvFyE4",
+desc:"Learn HTML5", lessons:["Tags","Forms","Media"]
+},
+
+{ id:2, title:"CSS3",
+img:"https://i.ytimg.com/vi/QhD7ZAvBkgE/hq720.jpg",
+desc:"Design UI", lessons:["Flexbox","Grid","Animations"]
+},
+
+{ id:3, title:"JavaScript",
+img:"https://repository-images.githubusercontent.com/161459736/524e6f00-8313-11e9-9c74-51546bca55a3",
+desc:"Learn JS", lessons:["DOM","Events","Async"]
+},
+
+{ id:4, title:"Java",
+img:"https://images.icon-icons.com/2699/PNG/512/java_horizontal_logo_icon_167858.png",
+desc:"Core Java", lessons:["OOP","Collections"]
+},
+
+{ id:5, title:"Spring",
+img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Wc09Id27jnhjN2R9vn8-ynwSKXCBwzSzPQ&s",
+desc:"Spring Framework", lessons:["Core","MVC"]
+},
+
+{ id:6, title:"Spring Boot",
+img:"https://e4developer.com/wp-content/uploads/2018/01/spring-boot.png",
+desc:"Backend apps", lessons:["REST","Security"]
+},
+
+{ id:7, title:"Hibernate",
+img:"https://upload.wikimedia.org/wikipedia/commons/2/22/Hibernate_logo_a.png",
+desc:"ORM Tool", lessons:["Mapping","Queries"]
+},
+
+{ id:8, title:"MySQL",
+img:"https://images.icon-icons.com/2699/PNG/512/mysql_horizontal_logo_icon_170929.png",
+desc:"Database", lessons:["Queries","Joins"]
+},
+
+{ id:9, title:"Python",
+img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0pnnlQSQeLJyezYqvbw4SYTHGlkS-DaV5dg&s",
+desc:"Python Programming", lessons:["Basics","OOP"]
+},
+
+{ id:10, title:".NET",
+img:"https://www.pngmart.com/files/23/Net-Logo-PNG-Image.png",
+desc:"Microsoft stack", lessons:["C#","ASP.NET"]
+},
+
+{ id:11, title:"React",
+img:"https://images.icon-icons.com/2699/PNG/512/reactjs_logo_icon_170805.png",
+desc:"Frontend lib", lessons:["Hooks","State"]
+},
+
+{ id:12, title:"Bootstrap",
+img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOqiXAYG6br3mjLDDR_kfKaWo2r6RBR58AJA&s",
+desc:"UI Framework", lessons:["Grid","Components"]
+},
+
+{ id:13, title:"Data Structures & Algorithms",
+img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdax9FRluoqUngwrRNuiFu0_UMyvEZRKqlxg&s",
+desc:"Problem solving", lessons:["Arrays","Trees"]
+},
+
+{ id:14, title:"System Design",
+img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9w0PldD6INwAvqpaHYx14dy49SjPQj93dA&s",
+desc:"Design systems", lessons:["Scaling","Architecture"]
+}
+
+];
